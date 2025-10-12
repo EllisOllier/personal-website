b4a7b3aa6d12a2ff8ee32c69144451240485c014
EllisOllier <ellisollier1@gmail.com>
2025-10-12 11:34:23 +0100

    Commented out ContactForm import due to build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // Development Branch
 import Image from "next/image";
-import ContactForm from "@/components/ContactForm"; // Need to work on it first
+//import ContactForm from "@/components/ContactForm"; // Need to work on it first
 import ProjectCard from "@/components/ProjectCard";
 import SkillCard from "@/components/SkillCard";
 import Link from "next/link";
