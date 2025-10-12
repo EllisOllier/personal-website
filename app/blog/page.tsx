0e27fe0fd5ce0a53c468ef58a8119ec6eff7f857
EllisOllier <ellisollier1@gmail.com>
2025-10-12 11:34:48 +0100

    Replaced <a> tag with <Link> tag

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link"
+
 export default function Blog(){
     return(
         <div className="text-white bg-gray-900 h-screen flex flex-col justify-center p-10">
             <h1 className="text-6xl" >Blog page is currently being built!</h1>
-            <a href="/" className="text-xl bg-green-500 w-full p-2 text-center rounded-sm">Back</a>
-            
+            <Link href={"/"} className="text-xl bg-green-500 w-full p-2 text-center rounded-sm">Back</Link>
         </div>
     )
 }
