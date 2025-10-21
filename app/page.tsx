115431d904e2357d76084020753281bd229d4ce6
EllisOllier <ellisollier1@gmail.com>
2025-10-21 11:59:48 +0100

    Updated about me on main page

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
                                 alt="A picture of me!"
                             />
                             <p className="max-w-100">
-                                Im Ellis, A Second-year Software Engineering
+                                I'm Ellis, A second-year Software Engineering
                                 student at Manchester Metropolitan University
                                 seeking a placement starting Summer 2026.
                                 Experience in leading a six-person team to
